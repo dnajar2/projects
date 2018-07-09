d17ab1b5fb72ad1f4a103e290e358ba8ccf25448
David Najar <dnajar2@gmail.com>
2018-07-09 09:20:38 -0700

    test scripts updates

--- html/includes/data.Class.php
+++ html/includes/data.Class.php
@@ -4,12 +4,13 @@ Process data coming in
 */
 class data{
   public $return = null;
+  public $res = null;
 
   public function checkPost($post){
-
     if(!empty($post)){
         $this->return = true;
     }
+    return $this->return;
   }
   public function res(){
     $res = new stdClass;
@@ -18,5 +19,3 @@ class data{
   }
 
 }
-
- ?>
